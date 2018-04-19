cc9571f481e8c350d57cdb9d0b950e497f6e4177
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-04-19 19:06:35 +0500

    add new features

--- contacts/OfferFabric.sol
+++ contacts/OfferFabric.sol
@@ -1,25 +1,94 @@
 pragma solidity ^0.4.20;
 
 import "./Utils.sol";
-// import "./Entities.sol";
 
 contract OfferFabric is Ownable {
+
+    event NewOffer(uint offerId, uint fundId, string name, string desc);
+
     enum State { Initial, Open, Failed, Paid, Refund }
 
     struct Offer {
-        address owner;
         string name;
+        string desc;
         State state;
         uint totalSum;
         uint maxSumPer;
         uint minSumPer;
-        uint fundId;
+    }
+
+    modifier ownerOfOffer(uint _offerId) {
+        require(msg.sender == offerToOwner[_offerId]);
+        _;
     }
     
-    Offer[] offers;
+    Offer[] public offers;
+
+    mapping (uint => address) public offerToOwner;
+    mapping (address => uint) ownerOfferCount;
+    mapping (uint => uint) public offerToFund;
+    mapping (uint => uint) fundOfferCount;
     
-    // function kill() public {
-    //     if(msg.sender == owner) selfdestruct(owner);
-    // }
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    function createOffer(uint _fundId, string _name, string _desc) external {
+        //TODO: add {require} for check fundId exists
+        uint offerId = offers.push(Offer(_name, _desc, State.Initial, 0, 0, 0)) - 1;
+        offerToOwner[offerId] = msg.sender;
+        ownerOfferCount[msg.sender]++;
+        offerToFund[offerId] = _fundId;
+        fundOfferCount[_fundId]++;
+        NewOffer(offerId, _fundId, _name, _desc);
+    }
+
+    function changeName(uint _offerId, string _newName) external ownerOfOffer(_offerId) {
+        offers[_offerId].name = _newName;
+    }
+
+    function changeDesc(uint _offerId, string _newDesc) external ownerOfOffer(_offerId) {
+        offers[_offerId].desc = _newDesc;
+    }
+
+    function changeState(uint _offerId, State _newState) external ownerOfOffer(_offerId) {
+        offers[_offerId].state = _newState;
+    }
+
+    function changeTotalSum(uint _offerId, uint _newTotalSum) external ownerOfOffer(_offerId) {
+        offers[_offerId].totalSum = _newTotalSum;
+    }
+
+    function changeMaxSumPer(uint _offerId, uint _newMaxSumPer) external ownerOfOffer(_offerId) {
+        offers[_offerId].maxSumPer = _newMaxSumPer;
+    }
+
+    function changeMinSumPer(uint _offerId, uint _newMinSumPer) external ownerOfOffer(_offerId) {
+        offers[_offerId].minSumPer = _newMinSumPer;
+    }
+
+    function getOffersByOwner(address _owner) external view returns (uint[]) {
+        uint[] memory result = new uint[](ownerOfferCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < offers.length; i++) {
+            if (offerToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
+
+    function getOffersByFundId(uint _fundId) external view returns (uint[]) {
+        uint[] memory result = new uint[](fundOfferCount[_fundId]);
+        uint counter = 0;
+        for (uint i = 0; i < offers.length; i++) {
+            if (offerToFund[i] == _fundId) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
     
 }
