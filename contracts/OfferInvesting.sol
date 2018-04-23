a31aa72ddfd2a16bdda0f061aa668e49bacf3ce5
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-04-23 13:59:59 +0500

    add modifier onlyInvestors

--- contracts/OfferInvesting.sol
+++ contracts/OfferInvesting.sol
@@ -4,13 +4,27 @@ import "./OfferFabric.sol";
 
 contract OfferInvesting is OfferFabric {
 
+    modifier onlyInvestors(uint _offerId) {
+        if (inInvestors(_offerId)) _;
     }
 
     mapping (uint => address[]) public offerToInvestors;
 
+    function inInvestors(uint _offerId) public view returns(bool) {
+        address[] memory investors = offerToInvestors[_offerId];
+        for (uint i = 0; i < investors.length; i++) {
+            if (investors[i] == msg.sender)
+                return true;
+        }
 
+        return false;
+    }
 
-    function invest(uint _offerId) external payable {
+    function inviteInvestors(uint _offerId, address[] _investors) external ownerOfOffer(_offerId) {
+        offerToInvestors[_offerId] = _investors;
+    }
+
+    function invest(uint _offerId) external payable onlyInvestors(_offerId) {
         Offer memory offer = offers[_offerId];
         require(msg.value >= offer.minSumPer);
         require(msg.value <= offer.maxSumPer);
