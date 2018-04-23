451d18ec47cadf3042b5d0e2b68c8ea0baae5eff
Alexey Shagaleev <alexey.shagaleev@yandex.ru>
2018-04-23 13:59:46 +0500

    simplification investment

--- contracts/OfferInvesting.sol
+++ contracts/OfferInvesting.sol
@@ -3,24 +3,12 @@ pragma solidity ^0.4.20;
 import "./OfferFabric.sol";
 
 contract OfferInvesting is OfferFabric {
-    
-    struct Investor {
-        address _address;
+
     }
 
-    struct Deposit {
-        uint sum;   //wei
-        // uint rate;
-    }
+    mapping (uint => address[]) public offerToInvestors;
 
-    mapping (uint => uint[]) offerToInvestors;
 
-    Investor[] public investors;
-
-    function inviteInvestor(uint _offerId, address _investor) external ownerOfOffer(_offerId) {
-        uint investorId = investors.push(Investor(_investor)) - 1;
-        offerToInvestors[_offerId].push(investorId);
-    }
 
     function invest(uint _offerId) external payable {
         Offer memory offer = offers[_offerId];
